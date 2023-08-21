f2b524b53f3c1123810122b389eb6f8806a8ff1f
Programmer628 <128913408+Programmer628@users.noreply.github.com>
2023-08-21 09:05:15 -0700

    Update RollerCoaster.java
    Are you tall enough to ride?

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,11 +5,18 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
-		
+		public static void main(args){
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+		String heightToRide = JOptionPane.showInputDialog("What is your height in inches?");
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+			int heightAsInt = Integer.parseInt(heightToRide);
+			if( heightAsInt > 42 ) {
+				JOptionPane.showMessageDialog(null, "Hurray! You are tall enough to ride the coaster alone!");
+			} else if( heightAsInt = 42 ){
+				JOptionPane.showMessageDialog(null, "Great job! You can ride the coaster, but you have to ride with someone else.");
+			} else if( heightAsInt < 42 ){
+				JOptionPane.showMessageDialog(null, "So sorry. You aren't tall enough to ride.");
+			}
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
@@ -22,4 +29,5 @@ public class RollerCoaster {
 		// "You can ride the coaster with someone else!" 
 	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-} 
+	} 
+}
